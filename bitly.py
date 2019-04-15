d72440253e891a3ba9356f22f81917056cc7e81a
Serg4356 <asdfg111000@yandex.ru>
2019-04-15 18:13:16 +0500

    some changes

--- bitly.py
+++ bitly.py
@@ -1,7 +1,22 @@
 import requests
 import os
 import sys
+from dotenv import load_dotenv
+import argparse
 
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description='''
+                    Program creates short bitly link
+                    if url inputted by user is not shortified yet,
+                    and returns counts of redirects if url is already added
+                    '''
+    )
+    parser.add_argument(
+        'link',
+        help='Enter your short bitly link or long url')
+    return parser
 
 
 def get_bitlink(token, long_url):
@@ -45,19 +60,21 @@ def is_bitlink(token, link):
 
 
 if __name__ == '__main__':
-  token = os.getenv('TOKEN')
-  users_link = input('Enter your link:\n')
-  if is_bitlink(token, users_link):
-    print('Redirects count is: {}'.format(
-      get_bitlink_redirect_count(
-        token,
-        users_link
-        ).json()['total_clicks']
-      ))
-  else:
-    try:
-      response = get_bitlink(token, users_link)
-      response.raise_for_status()
-      print('Short link: {}'.format(response.json()['link']))
-    except requests.exceptions.HTTPError:
-      print('WARNING!\n{}'.format(response.json()['message']))
+    load_dotenv()
+    token = os.getenv('TOKEN')
+    parser = create_parser()
+    users_link = parser.parse_args().link
+    if is_bitlink(token, users_link):
+      print('Redirects count is: {}'.format(
+        get_bitlink_redirect_count(
+          token,
+          users_link
+          ).json()['total_clicks']
+        ))
+    else:
+      try:
+        response = get_bitlink(token, users_link)
+        response.raise_for_status()
+        print('Short link: {}'.format(response.json()['link']))
+      except requests.exceptions.HTTPError:
+        print('WARNING!\n{}'.format(response.json()['message']))
